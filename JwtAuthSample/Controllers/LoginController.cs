d47666e5d310344974e3a16607a249b3c1440858
hamedfathi <noeffect@outlook.com>
2020-09-15 11:21:47 +0430

    init

---
+++ JwtAuthSample/Controllers/LoginController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JwtAuthSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtAuthSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string userName, string password)
+        {
+            User login = new User();
+            login.UserName = userName;
+            login.Password = password;
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+
+            if(user != null)
+            {
+                var token = GenerateJwt(user);
+                response = Ok(new
+                {
+                    token
+                });
+            }
+
+            return response;
+        }
+
+        private User AuthenticateUser(User login)
+        {
+            User user = null;
+            if (login.UserName == "hamed" && login.Password == "1234")
+            {
+                user = new User
+                {
+                    UserName = "hamed",
+                    Password = "1234",
+                    EmailAddress = "hamed@gmail.com"
+                };
+            }
+            return user;
+        }
+
+        private string GenerateJwt(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email,user.EmailAddress),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            var encodedToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodedToken;
+        }
+    }
+}
---
+++ JwtAuthSample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JwtAuthSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost("Post")]
+        public string Post()
+        {
+            var id = HttpContext.User.Identity as ClaimsIdentity;
+            var claims = id.Claims.ToList();
+            var userName = claims[0].Value;
+            return $"Welcome to '{userName}'!";
+        }
+    }
+}
